8a44298f720fca4173246bcd819f8813458405aa
ismaelAD <ismael.diakite@final.edu.tr>
2025-09-08 22:57:01 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if(config('app.env') === 'production') {
+        URL::forceScheme('https');
+        }
         Vite::prefetch(concurrency: 3);
         
         Inertia::share([
